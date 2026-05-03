26f78ea487776d55629c89ab76e2e78089fcd427
Lemka6ix <klider.fire@mail.ru>
2026-05-03 17:39:03 +0500

    Дописан класс Vector3D: - префиксный и постфиксный инкремент и декремент; - составные операторы присваивания; - бинарные арифметические операторы; - операторы сравнения векторов.

--- main.cpp
+++ main.cpp
@@ -42,4 +42,113 @@ class Vector3D {
             cnt--;
             std::cout << "The vector has been deleted. Left: " << cnt << std::endl;
         }
-}
+
+
+        void print() const {
+            std::cout << "x: " << x << " y: " << y << " z: " << z;
+        }
+
+
+        double vector_length() const {
+        return sqrt(x*x + y*y + z*z);
+        }
+
+        static int getCount() {
+        return cnt;
+        }
+
+        Vector3D& operator++() {
+        double percent = 1.01;
+        x *= percent;
+        y *= percent;
+        z *= percent;
+        return *this;
+        }
+
+        Vector3D operator++(int) {
+        Vector3D temp = *this;
+        double percent = 1.01;
+        x *= percent;
+        y *= percent;
+        z *= percent;
+        return temp;
+        }
+
+        Vector3D& operator--() {
+        double percent = 1.01;
+        x /= percent;
+        y /= percent;
+        z /= percent;
+        return *this;
+        }
+
+        Vector3D operator--(int) {
+        Vector3D temp = *this;
+        double percent = 1.01;
+        x /= percent;
+        y /= percent;
+        z /= percent;
+        return temp;
+        }
+
+        Vector3D& operator+=(const Vector3D& other) {
+        x += other.x;
+        y += other.y;
+        z += other.z;
+        return *this;
+        }
+
+        Vector3D& operator-=(const Vector3D& other) {
+        x -= other.x;
+        y -= other.y;
+        z -= other.z;
+        return *this;
+        }
+
+        Vector3D& operator*=(const Vector3D& other) {
+        double result = x * other.x + y * other.y + z * other.z;
+        x = result;
+        y = result;
+        z = result;
+        return *this;
+        }
+
+        friend Vector3D operator+(Vector3D lhs, const Vector3D& rhs) {
+        lhs += rhs;
+        return lhs;
+        }
+
+        friend Vector3D operator-(Vector3D lhs, const Vector3D& rhs) {
+        lhs -= rhs;
+        return lhs;
+        }
+
+        friend Vector3D operator*(Vector3D lhs, const Vector3D& rhs) {
+        lhs *= rhs;
+        return lhs;
+        }
+
+        bool operator<(const Vector3D& other) const {
+        return this->vector_length() < other.vector_length();
+        }
+
+        bool operator>(const Vector3D& other) const {
+        return this->vector_length() > other.vector_length();
+        }
+
+        bool operator<=(const Vector3D& other) const {
+        return this->vector_length() <= other.vector_length();
+        }
+
+        bool operator>=(const Vector3D& other) const {
+        return this->vector_length() >= other.vector_length();
+        }
+
+        bool operator==(const Vector3D& other) const {
+        return this->vector_length() == other.vector_length();
+        }
+
+         bool operator!=(const Vector3D& other) const {
+        return this->vector_length() != other.vector_length();
+        }
+};
