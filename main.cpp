c9c851966144d14310ab5dfc074862f5f105af95
Lemka6ix <klider.fire@mail.ru>
2026-05-06 13:16:53 +0500

    - Добавлена инициализация статической переменной-члена; - Главное меню выора для пользователя; - createVector который создает новый вектор на основе ввода пользователя.

--- main.cpp
+++ main.cpp
@@ -152,3 +152,51 @@ class Vector3D {
         return this->vector_length() != other.vector_length();
         }
 };
+
+
+int Vector3D::cnt = 0;
+
+int menu(int choice) {
+    std::cout << "\n1. Create vector\n"
+              << "2. Show all vectors\n"
+              << "3. Vector addition\n"
+              << "4. Vector subtraction\n"
+              << "5. Vector multiplication\n"
+              << "6. Increase vector by 1%\n"
+              << "7. Decrease vector by 1%\n"
+              << "8. Compare vector lengths\n"
+              << "9. Show total vector count\n"
+              << "0. Exit" << std::endl;
+    std::cout << "Your choice: ";
+    std::cin >> choice;
+    return choice;
+}
+
+
+Vector3D createVector() {
+    int method;
+    std::cout << "Choose creation method:\n";
+    std::cout << "1. By coordinates (x, y, z)\n";
+    std::cout << "2. By two points (x1, y1, z1, x2, y2, z2)\n";
+    std::cout << "Your choice: ";
+    std::cin >> method;
+    
+    if (method == 1) {
+        double x, y, z;
+        std::cout << "Enter vector coordinates (x y z): ";
+        std::cin >> x >> y >> z;
+        return Vector3D(x, y, z);
+    }
+    else if (method == 2) {
+        double x1, y1, z1, x2, y2, z2;
+        std::cout << "Enter first point (x1 y1 z1): ";
+        std::cin >> x1 >> y1 >> z1;
+        std::cout << "Enter second point (x2 y2 z2): ";
+        std::cin >> x2 >> y2 >> z2;
+        return Vector3D(x1, y1, z1, x2, y2, z2);
+    }
+    else {
+        std::cout << "Invalid choice! Creating default vector.\n";
+        return Vector3D();
+    }
+}
